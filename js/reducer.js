196d2db3d010289b975d1ced057c5bb5c780b68c
Thomas Muscarello <muscarello.thomas@gmail.com>
2021-03-15 20:01:02 -0400

    Done.

--- js/reducer.js
+++ js/reducer.js
@@ -1,6 +1,8 @@
-let state = {count: 0};
+//At the top of the file, we declare but do not assign our state, so it starts off undefined.
+let state;
 
-function changeState(state, action){
+//changeState() is executed, passing through two local variables: state and action
+function changeState(state= { count: 0 }, action){
     switch (action.type) {
       case 'INCREASE_COUNT':
         return {count: state.count + 1}
@@ -9,6 +11,7 @@ function changeState(state, action){
     }
   }
 
+//dispatch() calls the changeState() reducer.
 function dispatch(action){
     state = changeState(state, action)
     render()
@@ -17,3 +20,6 @@ function dispatch(action){
 function render(){
     document.body.textContent = state.count
 }
+
+//At the bottom the file, we dispatch an action of '@@INIT'. This calls our dispatch() function and passes through our initial action
+dispatch({ type: '@@INIT' })
